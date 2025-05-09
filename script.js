f9a484ca98890310df400a985faedf2cd0309507
IgorEfimof <82845934+IgorEfimof@users.noreply.github.com>
2025-05-09 08:35:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-1
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card img");
+
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      const imgWindow = window.open("", "_blank");
+      imgWindow.document.write(
+        `<html><head><title>${card.alt}</title></head><body style='margin:0;text-align:center;background:#000;'>` +
+        `<img src='${card.src}' style='max-width:100%;max-height:100vh;'>` +
+        `</body></html>`
+      );
+    });
+  });
+});
+
